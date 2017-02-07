ab5cba92322b6d045465ec3da9fbd849b01586ba
Robert Todea <robert.todea@e-spres-oh.com>
2017-02-07 03:29:14 +0200

    More bubles for each CSV

--- client/src/app/esri-map/esri-map.component.ts
+++ client/src/app/esri-map/esri-map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, ViewChild, ElementRef, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, ViewChild, ElementRef, Output, Input } from '@angular/core';
 
 import { EsriService } from './esri.service';
 
@@ -10,169 +10,213 @@ import { EsriService } from './esri.service';
 export class EsriMapComponent implements OnInit {
   @ViewChild('map') mapEl: ElementRef;
 
+  @Input() userType: string;
   @Output() onMapEvent = new EventEmitter<any>();
 
   mapView: __esri.MapView;
   map: __esri.Map;
   locator: __esri.Locator;
 
-  constructor(private esriService: EsriService) {
+  mapCreatorForUserType = {
+    patient: createPatientMap,
+    donor: createDonorMap
+  };
+
+  constructor(public esriService: EsriService) {
   }
 
   ngOnInit() {
     // only load the ArcGIS API for JavaScript when this component is loaded
     return this.esriService.isLoaded.then(() => {
-      this.create();
+      this.mapCreatorForUserType[this.userType](this);
     });
   }
+}
 
-  private create() {
-    this.map = new this.esriService.Map({basemap: 'streets'});
+function createMapView(mapComponent: EsriMapComponent) {
+  mapComponent.mapView = new mapComponent.esriService.views.MapView({
+    container: mapComponent.mapEl.nativeElement.id,
+    map: mapComponent.map,
+    zoom: 4,
+    center: [22, 45]  // Sets the center point of view in lon/lat
+  });
+}
 
-    this.addCSVFeatureLayer();
+function createDonorMap(mapComponent: EsriMapComponent) {
+  mapComponent.map = new mapComponent.esriService.Map({basemap: 'streets'});
 
-    this.mapView = new this.esriService.views.MapView({
-      container: this.mapEl.nativeElement.id,
-      map: this.map,
-      zoom: 4,
-      center: [22, 45]  // Sets the center point of view in lon/lat
-    });
+  createMapView(mapComponent);
 
-    this.initReverseLocator();
-    this.addMapWidgets();
-    this.addPopupTemplate();
-  }
+  initReverseLocator(mapComponent);
+  addMapWidgets(mapComponent);
+  addLocatePopupTemplate(mapComponent);
+}
 
-  // TODO: this does not work for some unknown reason...
-  private addFeatureLayer() {
-    const renderer = new this.esriService.renderers.SimpleRenderer({
-      symbol: new this.esriService.symbols.SimpleMarkerSymbol({
-        style: 'circle',
-        size: '20px',
-        color: '#673ab7',
-        outline: { // autocasts as new SimpleLineSymbol()
-          color: [255, 64, 0, 0.4], // autocasts as new Color()
-          width: 7
-        }
-      })
-    });
+function createPatientMap(mapComponent: EsriMapComponent) {
+  mapComponent.map = new mapComponent.esriService.Map({basemap: 'streets'});
 
-    const layer = new this.esriService.layers.FeatureLayer({
-        source: [
-          {
-            geometry: new this.esriService.geometry.Point({
-              x: 2719864.8299347507,
-              y: 6058490.576041063}),
-            attributes: {
-              ObjectId: 0
-            }
-          }
-        ],
-        fields: [
-          {
-            name: 'ObjectID',
-            alias: 'ObjectID',
-            type: 'oid'
-          }
-        ],
-        objectIdField: 'ObjectID',
-        spatialReference: {
-          wkid: 4326
-        },
-        geometryType: 'point',
-        renderer,
+  addCSVFeatureLayer(mapComponent);
+
+  createMapView(mapComponent);
+
+  initReverseLocator(mapComponent);
+  addMapWidgets(mapComponent);
+  // addDetailsPopupAction(mapComponent);
+}
+
+// TODO: this does not work for some unknown reason...
+function addFeatureLayer(mapComponent) {
+  const renderer = new mapComponent.esriService.renderers.SimpleRenderer({
+    symbol: new mapComponent.esriService.symbols.SimpleMarkerSymbol({
+      style: 'circle',
+      size: '20px',
+      color: '#673ab7',
+      outline: { // autocasts as new SimpleLineSymbol()
+        color: [255, 64, 0, 0.4], // autocasts as new Color()
+        width: 7
       }
-    );
+    })
+  });
 
-    this.map.add(layer);
-  }
-
-  // TODO: backup because the dynamic one above does not work
-  private addCSVFeatureLayer() {
-    const url = '/api/map/dump';
-
-    const csvLayer = new this.esriService.layers.CSVLayer({ url });
-
-    csvLayer.renderer = new this.esriService.renderers.SimpleRenderer({
-      symbol: new this.esriService.symbols.SimpleMarkerSymbol({
-        size: '23px',
-        color: [238, 69, 0, 0.5],
-        outline: {
-          width: 0.5,
-          color: 'white'
+  const layer = new mapComponent.esriService.layers.FeatureLayer({
+      source: [
+        {
+          geometry: new mapComponent.esriService.geometry.Point({
+            x: 2719864.8299347507,
+            y: 6058490.576041063}),
+          attributes: {
+            ObjectId: 0
+          }
         }
-      })
-    });
+      ],
+      fields: [
+        {
+          name: 'ObjectID',
+          alias: 'ObjectID',
+          type: 'oid'
+        }
+      ],
+      objectIdField: 'ObjectID',
+      spatialReference: {
+        wkid: 4326
+      },
+      geometryType: 'point',
+      renderer,
+    }
+  );
 
-    this.map.add(csvLayer);
-  }
+  mapComponent.map.add(layer);
+}
 
-  private initReverseLocator() {
-    this.locator = new this.esriService.tasks.Locator({
-      url: 'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer'
-    });
-  }
+// TODO: backup because the dynamic one above does not work
+function addCSVFeatureLayer(mapComponent) {
+  const csvLayer = new mapComponent.esriService.layers.CSVLayer({
+    url: '/api/map/dump',
+    popupTemplate: {
+      title: 'Info',
+      content: '{mag} {type}'
+    }
+  });
 
-  private addMapWidgets() {
-    const searchWidget = new this.esriService.widgets.Search({view: this.mapView});
+  csvLayer.renderer = new mapComponent.esriService.renderers.SimpleRenderer({
+    symbol: new mapComponent.esriService.symbols.SimpleMarkerSymbol({
+      size: '23px',
+      color: [238, 69, 0, 0.5],
+      outline: {
+        width: 0.5,
+        color: 'white'
+      }
+    })
+  });
 
-    this.mapView.ui.add(searchWidget, {
-      position: 'top-left',
-      index: 0
-    });
+  mapComponent.map.add(csvLayer);
+}
 
-    const locateBtn = new this.esriService.widgets.Locate({view: this.mapView});
+function initReverseLocator(mapComponent) {
+  mapComponent.locator = new mapComponent.esriService.tasks.Locator({
+    url: 'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer'
+  });
+}
 
-    // Add the locate widget to the top left corner of the view
-    this.mapView.ui.add(locateBtn, {
-      position: 'top-left',
-      index: 1
-    });
-  }
+function addMapWidgets(mapComponent) {
+  const searchWidget = new mapComponent.esriService.widgets.Search({view: mapComponent.mapView});
 
-  addPopupTemplate() {
-    const locationDetails = { mapPoint: null, address: null };
+  mapComponent.mapView.ui.add(searchWidget, {
+    position: 'top-left',
+    index: 0
+  });
 
-    this.mapView.on('click', ({mapPoint}) => {
-      this.locator.locationToAddress(mapPoint)
-        .then((response) => {
-          this.mapView.popup.open({
-            title: 'Set current location',
-            location: mapPoint,
-            content: response.address.Match_addr
-          });
-          // this.onMapEvent.emit({eventType: 'click', address: response.address, mapPoint});
-          locationDetails.mapPoint = mapPoint;
-          locationDetails.address = response.address;
-        })
-        .otherwise(() => {
-          this.mapView.popup.open({
-            title: `Set current location to [Lon: ${mapPoint.longitude}, Lat: ${mapPoint.latitude}]`,
-            location: mapPoint,
-            content: 'No address was found for this location'
-          });
-          locationDetails.mapPoint = mapPoint;
-          locationDetails.address = null;
-          // this.onMapEvent.emit({eventType: 'click', mapPoint});
+  const locateBtn = new mapComponent.esriService.widgets.Locate({view: mapComponent.mapView});
+
+  // Add the locate widget to the top left corner of the view
+  mapComponent.mapView.ui.add(locateBtn, {
+    position: 'top-left',
+    index: 1
+  });
+}
+
+function addLocatePopupTemplate(mapComponent) {
+  const locationDetails = { mapPoint: null, address: null };
+
+  mapComponent.mapView.on('click', ({mapPoint}) => {
+    mapComponent.locator.locationToAddress(mapPoint)
+      .then((response) => {
+        mapComponent.mapView.popup.open({
+          title: 'Set current location',
+          location: mapPoint,
+          content: response.address.Match_addr
         });
-    });
+        // mapComponent.onMapEvent.emit({eventType: 'click', address: response.address, mapPoint});
+        locationDetails.mapPoint = mapPoint;
+        locationDetails.address = response.address;
+      })
+      .otherwise(() => {
+        mapComponent.mapView.popup.open({
+          title: `Set current location to [Lon: ${mapPoint.longitude}, Lat: ${mapPoint.latitude}]`,
+          location: mapPoint,
+          content: 'No address was found for this location'
+        });
+        locationDetails.mapPoint = mapPoint;
+        locationDetails.address = null;
+        // mapComponent.onMapEvent.emit({eventType: 'click', mapPoint});
+      });
+  });
 
-    const moreDetailsAction = {
-      // This text is displayed as a tool tip
-      title: 'Register',
-      id: 'register',
-      className: 'esri-icon-authorize'
-    };
+  const registerAction = {
+    // This text is displayed as a tool tip
+    title: 'Register',
+    id: 'register',
+    className: 'esri-icon-authorize'
+  };
 
-    // Adds the action to the view's default popup.
-    this.mapView.popup.actions.push(moreDetailsAction);
+  // Adds the action to the view's default popup.
+  mapComponent.mapView.popup.actions.push(registerAction);
 
-    // event handler that fires each time an action is clicked
-    this.mapView.popup.on('trigger-action', (event) => {
-      /* If the zoom-out action is clicked, the following code executes  */
-      if (event.action.id === 'register') {
-        this.onMapEvent.emit({eventType: event.action.id, mapPoint: locationDetails.mapPoint, address: locationDetails.address });
-      }
-    });
-  }
+  // event handler that fires each time an action is clicked
+  mapComponent.mapView.popup.on('trigger-action', (event) => {
+    /* If the zoom-out action is clicked, the following code executes  */
+    if (event.action.id === 'register') {
+      mapComponent.onMapEvent.emit({eventType: event.action.id, mapPoint: locationDetails.mapPoint, address: locationDetails.address });
+    }
+  });
+}
+
+function addDetailsPopupAction(mapComponent) {
+  const moreDetailsAction = {
+    // This text is displayed as a tool tip
+    title: 'Details',
+    id: 'details',
+    className: 'esri-icon-locked'
+  };
+
+  // Adds the action to the view's default popup.
+  mapComponent.mapView.popup.actions.push(moreDetailsAction);
+
+  // event handler that fires each time an action is clicked
+  mapComponent.mapView.popup.on('trigger-action', (event) => {
+    /* If the zoom-out action is clicked, the following code executes  */
+    if (event.action.id === 'details') {
+      mapComponent.onMapEvent.emit({ eventType: event.action.id });
+    }
+  });
 }
