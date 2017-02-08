17aafe21c91853e14fbd466d473e12a731fefa5b
Robert Todea <robert.todea@e-spres-oh.com>
2017-02-09 01:49:44 +0200

    Add GeoJSON support

--- server/routes/map-service.js
+++ server/routes/map-service.js
@@ -1,15 +1,40 @@
 const json2csv = require("json2csv");
-const { DonorModel, jsonifier } = require('../models/donor');
+const GeoJSON = require('geojson');
+
+const { DonorModel, jsonifier, DONOR_REST_FIELDS } = require('../models/donor');
 
 function registerMapEndpoints(router) {
-  console.log('Registering Custom Map Server: ', '/map/zoom/x/y.csv');
-  router.get('/map/:z/:x/:y.csv', (_, res) => {
-    res.set('Content-Type', 'text/csv');
+  const fetchAllDonors = () => DonorModel.find().then(data => {
+    return data.map(jsonifier);
+  });
 
-    DonorModel.find().then((persistedDonors) => (persistedDonors || []).map(jsonifier))
-      .then((persistedDonors) => {
-      res.send(json2csv({ data: persistedDonors, quotes: '' }));
-    });
+  registerDataFormatEndpoint(router, 'csv', 'text/csv', fetchAllDonors,
+    (donorArray) => json2csv({ data: donorArray, quotes: '', fields: [].concat(DONOR_REST_FIELDS, ['id'])}),
+    {});
+
+  registerDataFormatEndpoint(router, 'geojson', 'application/json', fetchAllDonors,
+    (donorArray) => GeoJSON.parse(donorArray, {Point: ['latitude', 'longitude']}),
+    {});
+
+}
+
+function registerDataFormatEndpoint(router, formatName, mimeType, dataRetriever, formatter, filters) {
+  console.log('Registering Custom Map Server: ', `/map/zoom/x/y.${formatName}`);
+  router.get(`/map/:z/:x/:y.${formatName}`, (req, res) => {
+    res.set('Content-Type', mimeType);
+
+    // TODO: extend with some filter functionality
+    // { filterName: predicate }
+    let dataFilter;
+    if (!(req.query.filter)) {
+      dataFilter = () => true; // let everyone
+    }
+
+    dataRetriever()
+      .then((data) => data.filter(dataFilter))
+      .then((data) => {
+        res.send(formatter(data));
+      });
   });
 }
 
